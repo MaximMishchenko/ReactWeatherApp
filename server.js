8d02e2d2b93af2e2c760dc979984df03c13028fe
MaximMishchenko <maxim.mishenko@yandex.ru>
2017-03-13 19:58:02 +0200

    Update middleware for localhost

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ var express = require('express'),
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'http'){
-    next();
-  } else {
+  if(req.headers['x-forwarded-proto'] === 'https'){
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function (req, res){
-  response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
 app.listen(PORT, function(){
