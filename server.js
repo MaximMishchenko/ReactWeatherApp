466089cfbca812a837f8728f8b1007c45b17088e
MaximMishchenko <maxim.mishenko@yandex.ru>
2017-03-13 19:32:21 +0200

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ var express = require('express'),
     path = require('path'),
     app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', function (request, response){
+app.get('*', function (req, res){
   response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
-app.listen(3000, function(){
-  console.log('Server working...');
+app.listen(PORT, function(){
+  console.log('Server working on port ' + PORT);
 });
